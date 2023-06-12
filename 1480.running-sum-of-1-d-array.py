5f30136773ea2e0d402adf09b27d8657612cae64
Sidilesh Mallanna <arjunmallana@gmail.com>
2023-06-12 20:23:37 +0530

    Day-6-update

---
+++ 1480.running-sum-of-1-d-array.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=1480 lang=python
+#
+# [1480] Running Sum of 1d Array
+#
+
+# @lc code=start
+class Solution(object):
+    def runningSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+# @lc code=end
+
